65dd49cda8772f2ef39e31d4fda23a01d2d706c8
msafdev <salmanalfarisi261002@gmail.com>
2025-06-24 15:14:41 +0700

    feat: update

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     username: { type: String, required: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
@@ -11,12 +12,13 @@ const UserSchema = new mongoose.Schema({
     noHp: { type: String, required: true },
     alamat: { type: String, required: true },
     projects: { type: [String], required: true },
-    filename: { type: String, required: true },
-    filepath: { type: String, required: true },
+    avatarUser: { type: String },
     isSuperAdmin: { type: Boolean, required: true },
     isAdmin: { type: Boolean, required: true },
     isVerify: { type: Boolean, required: true },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 // Membuat index pada nodeId di koleksi Node untuk pencarian cepat
 UserSchema.index({ username: 1 });
