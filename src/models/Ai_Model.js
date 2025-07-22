85198264e0682016f7f452e7ac5b2e696a793dec
fairuzluthfa <fairuzluthfa@gmail.com>
2025-07-22 15:48:37 +0700

    based Ai_model

---
+++ src/models/Ai_Model.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose')
+
+const Ai_ModelSchema=new mongoose.Schema({
+    namaModel:{type:String,required:true,unique:true},
+    jenisModel:{type:String,required:true},
+    namaPembuat:{type:String,required:false},
+    fileName:{type:String,required:true},
+    notes:{type:String,required:false}
+
+},
+    {timestamps:true}
+)
+
+module.exports=mongoose.model("Ai_Model",Ai_ModelSchema)
